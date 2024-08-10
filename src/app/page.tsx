f78856d01bb200879433503dac27e7090cc2e589
v1xingyue <qixingyue@gmail.com>
2024-08-11 01:33:23 +0800

    update commit username
    Hash (sha256) : 65fe467c38e2de29761f45ad3aead4aef8e445f2ebb28dd6ef9c69677336c798
    Signature: 3bcc1fb58598ab9872cad8d80d5a99ac52e1a5e904516f1755ad894bbb17cc035a8f87875da6734d7e1bab1d96924a65db1d5b235131085da34d15446cdc140a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,6 +71,7 @@ export default function Home() {
             <td>Commit url </td>
             <td>Commit message </td>
             <td>Commit repo_url </td>
+            <td>Commit username </td>
           </tr>
           {commits.map((commit) => {
             let decode_value = commit.state.decoded_value.value.value.value;
@@ -80,6 +81,7 @@ export default function Home() {
                 <td>{decode_value.commit_url}</td>
                 <td>{decode_value.message}</td>
                 <td>{decode_value.repo_url}</td>
+                <td>{decode_value.commit_user}</td>
               </tr>
             );
           })}
