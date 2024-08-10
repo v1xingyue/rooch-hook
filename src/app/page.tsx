23ef6ce9f44544ff7ebc4843d65d1349a8f62768
v1xingyue <qixingyue@gmail.com>
2024-08-10 19:16:17 +0800

    display commits list
    Hash (sha256) : 3eeca22d8adbdebb89ba9d21b274ae057ca1da45e25fab4fc683caf46c94801f
    Signature: 1a152b44eae6ff7600792d1a6624d6411c847942b8fcca5cb6aa3ac1d696dcbbae597a0b0f93f29a5a0033a5aa6cdbe8f134005594516fd5bfd2ddf32aa9a106

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,84 @@
 "use client";
 
-import { useEffect } from "react";
-import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+import { getRoochNodeUrl, RoochClient } from "@roochnetwork/rooch-sdk";
+import "./page.css";
 
 export default function Home() {
-  useEffect(() => {
-    console.log("page.tsx");
-  });
-
   const mypackage = process.env.NEXT_PUBLIC_PACKAGE_ADDRESS;
   const network = process.env.NEXT_PUBLIC_NETWORK;
+
+  const [tableId, setTableId] = useState("");
+  const [commits, setCommits] = useState<any[]>([]);
+
+  useEffect(() => {
+    const loadCommits = async () => {
+      const client = new RoochClient({
+        url: getRoochNodeUrl(network as any),
+      });
+
+      const resource = await client.getStates({
+        accessPath: `/resource/${mypackage}/${mypackage}::developer::DeveloperInfo`,
+        stateOption: {
+          decode: true,
+        },
+      });
+
+      console.log(resource);
+
+      if (resource.length > 0) {
+        const v = resource[0].decoded_value?.value.value as any;
+        let table_id = v.value.commits.value.contents.value.handle.value
+          .id as string;
+        setTableId(table_id);
+      } else {
+        console.log("No resource found");
+      }
+    };
+    loadCommits();
+  }, [mypackage, network]);
+
+  useEffect(() => {
+    const loadCommits = async () => {
+      if (tableId) {
+        const client = new RoochClient({
+          url: getRoochNodeUrl(network as any),
+        });
+        const commits = await client.listStates({
+          accessPath: `/table/${tableId}`,
+          stateOption: {
+            decode: true,
+            showDisplay: false,
+          },
+        });
+        console.log(JSON.stringify(commits.data, null, 2));
+        setCommits(commits.data as any[]);
+      }
+    };
+    loadCommits();
+  }, [tableId, network]);
+
   return (
-    <main className={styles.main}>
+    <main className="main">
       <h3>
         Github Commit List: {mypackage} on {network}
       </h3>
-      <div></div>
+      <div>
+        <p>Table ID : {tableId}</p>
+        <table>
+          {commits.map((commit) => {
+            let decode_value = commit.state.decoded_value.value.value.value;
+            return (
+              <tr key={commit.field_key}>
+                <td>{decode_value.commit_time}</td>
+                <td>{decode_value.commit_url}</td>
+                <td>{decode_value.message}</td>
+                <td>{decode_value.repo_url}</td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     </main>
   );
 }
