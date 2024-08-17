13a6d203cf89741a3b07a702866f8ce1947cc818
v1xingyue <qixingyue@gmail.com>
2024-08-17 16:30:23 +0800

    test commit by url
    Address: 0x78201248fb1ea556f2dae22ffd5bfc119d6b0a0f7bfaa7846ee455fc43e008dc
    Hash (sha256) : f731211bb603c53ecba22d86bdbe2e05e90f038ab06d336665db2775a07bd5a6
    Signature: c856739642ababafc8c3044554783d50e0619345078f3a2430e54c041754a2259c7842a309bcbf8aa7e77b8e2a1e5c4799c5f480c78c5757eacbd27733067c03

--- src/app/api/github/hooks/push/route.ts
+++ src/app/api/github/hooks/push/route.ts
@@ -12,21 +12,20 @@ const parseCommitMessge = (msg: string) => {
   let parts = msg.split("\n\n");
   let messageBody = parts[0];
   let sign_parts = parts[1].split("\n");
-  if (sign_parts.length == 2) {
-    let msg_hash = String(sign_parts[0]).substring(16);
-    let msg_signate = String(sign_parts[1]).substring(11);
-    return {
-      messageBody,
-      msg_hash,
-      msg_signate,
-    };
-  } else {
+  if (sign_parts.length >= 3) {
     let count = sign_parts.length;
+    let commitAddress = String(sign_parts[count - 3])
+      .split(":")[1]
+      .trim();
     let msg_hash = String(sign_parts[count - 2])
       .split(":")[1]
       .trim();
-    let msg_signate = String(sign_parts[count - 1]).substring(11);
+    let msg_signate = String(sign_parts[count - 1])
+      .split(":")[1]
+      .trim();
+
     return {
+      commitAddress,
       messageBody,
       msg_hash,
       msg_signate,
@@ -41,7 +40,7 @@ export const POST = async (req: Request) => {
     const repo_url = data.repository.url as string;
     const commit_url = commit.url;
     const raw_message = commit.message as string;
-    const parsed = parseCommitMessge(raw_message);
+    const parsed = parseCommitMessge(raw_message)!;
     const commit_user = commit.committer.username;
 
     const pair = Secp256k1Keypair.fromSecretKey(process.env.PRIVATE_KEY!);
@@ -57,6 +56,7 @@ export const POST = async (req: Request) => {
     tx.callFunction({
       target: `${packageAddress}::developer::commit`,
       args: [
+        Args.address(parsed.commitAddress),
         Args.string(repo_url),
         Args.string(commit_url),
         Args.string(parsed.messageBody),
