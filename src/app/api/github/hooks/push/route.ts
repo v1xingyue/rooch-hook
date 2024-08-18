629e62085d261241153f6857ffd42b301ff5a171
v1xingyue <qixingyue@gmail.com>
2024-08-18 23:47:51 +0800

    display params
    Address: 0x7c411d395967b97630dc2b5aa4c635dc38735d74eea47ff2a317f906b95d6a6a
    Hash (sha256) : 3ccaba8a8e22591b763b84ef9e24e512ac50f3be0689bce26edb144e0ad27c48
    Signature: 1260849d252ce73805f1aad0413c41b3ae8603b1f5264e56daac771349c9d372d0182fc81c27feb4ae6829f75d88fd7a9ee276b1b241c1a0eabafee4d95e070c

--- src/app/api/github/hooks/push/route.ts
+++ src/app/api/github/hooks/push/route.ts
@@ -35,6 +35,7 @@ const parseCommitMessge = (msg: string) => {
 
 export const POST = async (req: Request) => {
   const data = await req.json();
+  let params = {};
   try {
     const commit = data.commits[0] as any;
     const repo_url = data.repository.url as string;
@@ -51,6 +52,19 @@ export const POST = async (req: Request) => {
     const client = new RoochClient({
       url,
     });
+
+    params = {
+      target: `${packageAddress}::developer::commit`,
+      args: [
+        parsed.commitAddress,
+        repo_url,
+        commit_url,
+        parsed.messageBody,
+        commit_user,
+        parsed.msg_signate,
+        parsed.msg_hash,
+      ],
+    };
 
     const tx = new Transaction();
     tx.callFunction({
@@ -74,6 +88,6 @@ export const POST = async (req: Request) => {
       result,
     });
   } catch (error) {
-    return Response.json({ error, data });
+    return Response.json({ error, params });
   }
 };
