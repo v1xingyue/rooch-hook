89c7582e597e7ace320c6701cb85eb32b96060c5
v1xingyue <qixingyue@gmail.com>
2024-08-19 13:56:17 +0800

    let me create session
    Address: 0x7c411d395967b97630dc2b5aa4c635dc38735d74eea47ff2a317f906b95d6a6a
    Hash (sha256) : a1602998ef5ae7472ed33e93b79e7fb46c8a5f6d003fbdca2f7cd86e87dab611
    Signature: 5dc71fba05d2138fbd8b215da2d6bc7dbb7af2b980a40459b562f461a2838f3ee8b3ff87fb419475a1393a706af130ec48dfd750a37c530776550b77e3246e09

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,19 @@
 import {
   useConnectWallet,
+  useCreateSessionKey,
   useCurrentAddress,
+  useCurrentSession,
+  useRemoveSession,
   useWallets,
   useWalletStore,
 } from "@roochnetwork/rooch-sdk-kit";
 import { shortAddress } from "./util";
 import Button from "@mui/material/Button";
 import { Link, Stack } from "@mui/material";
+import { useState } from "react";
 
 const Header = () => {
+  const mypackage = process.env.NEXT_PUBLIC_PACKAGE_ADDRESS;
   const currentAddress = useCurrentAddress();
   const wallets = useWallets();
   const connectionStatus = useWalletStore((state) => state.connectionStatus);
@@ -16,13 +21,70 @@ const Header = () => {
     (state) => state.setWalletDisconnected
   );
   const { mutateAsync: connectWallet } = useConnectWallet();
+  const session = useCurrentSession();
+  const [sessionLoading, setSessionLoading] = useState(false);
+
+  const { mutateAsync: createSessionKey } = useCreateSessionKey();
+  const { mutateAsync: removeSessionKey } = useRemoveSession();
+  const handlerCreateSessionKey = async () => {
+    if (sessionLoading) {
+      return;
+    }
+    setSessionLoading(true);
+
+    const defaultScopes = [`${mypackage}::*::*`];
+    createSessionKey(
+      {
+        appName: "rooch hook",
+        appUrl: location.href,
+        maxInactiveInterval: 1000,
+        scopes: defaultScopes,
+      },
+      {
+        onSuccess: (result) => {
+          console.log("session key", result);
+        },
+        onError: (why) => {
+          console.log(why);
+        },
+      }
+    ).finally(() => setSessionLoading(false));
+  };
 
   return (
     <Stack position="static" color="default" justifyContent="flex-end">
       <div>
-        <Link href="/" style={{ textDecoration: 'none',fontWeight:"bold" }} >Rooch Hooks</Link>
-        <span style={{float:"right"}}>
+        <Link href="/" style={{ textDecoration: "none", fontWeight: "bold" }}>
+          Rooch Hooks
+        </Link>
+        <span style={{ float: "right" }}>
+          {!session ? (
+            <Button
+              variant="contained"
+              sx={{ marginRight: "1rem" }}
+              onClick={handlerCreateSessionKey}
+            >
+              Init Session
+            </Button>
+          ) : (
+            <Button
+              color="warning"
+              sx={{ marginRight: "1rem" }}
+              variant="contained"
+              onClick={async () => {
+                console.log(session.getAuthKey());
+                const result = await removeSessionKey({
+                  authKey: session.getAuthKey(),
+                });
+                console.log(result);
+              }}
+            >
+              Clear Session
+            </Button>
+          )}
+
           <Button
+            color="primary"
             variant="contained"
             onClick={async () => {
               if (connectionStatus === "connected") {
