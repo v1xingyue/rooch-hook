9e723beb1949f80a480988fe48c5e7facc1ff12d
v1xingyue <qixingyue@gmail.com>
2024-08-22 01:39:43 +0800

    deal with more commits
    Address: 0xe42e88f0e87f799450c27e07d7d7d31ba0c379e007e6bdcfeb66964784360006
    Hash (sha256) : 829954715741b9ee47489b631336044b00f083344566e702c40c5d2cff8f62e5
    Signature: 8ab98df287e6fe6f3b02f5503f7a22d62f2aea95332c3bf490c6f401d101dbaeaa934e3735e2ad35c3a486e2a88993b40454ac84d4ed2b8306685f545ff0be03

--- src/app/api/github/hooks/push/route.ts
+++ src/app/api/github/hooks/push/route.ts
@@ -14,80 +14,86 @@ const parseCommitMessge = (msg: string) => {
   let sign_parts = parts[1].split("\n");
   if (sign_parts.length >= 3) {
     let count = sign_parts.length;
-    let commitAddress = String(sign_parts[count - 3])
-      .split(":")[1]
-      .trim();
-    let msg_hash = String(sign_parts[count - 2])
-      .split(":")[1]
-      .trim();
-    let msg_signate = String(sign_parts[count - 1])
-      .split(":")[1]
-      .trim();
-
-    return {
-      commitAddress,
-      messageBody,
-      msg_hash,
-      msg_signate,
-    };
+    let addressLine = sign_parts[count - 3];
+    if (addressLine.indexOf(":") !== -1) {
+      let commitAddress = addressLine.split(":")[1].trim();
+      let msgHashLine = sign_parts[count - 2];
+      if (msgHashLine.indexOf(":") !== -1) {
+        let msg_hash = msgHashLine.split(":")[1].trim();
+        let signatureLine = sign_parts[count - 1];
+        if (signatureLine.indexOf(":") !== -1) {
+          let msg_signate = signatureLine.split(":")[1].trim();
+          return {
+            commitAddress,
+            messageBody,
+            msg_hash,
+            msg_signate,
+          };
+        }
+      }
+    }
   }
 };
 
 export const POST = async (req: Request) => {
   const data = await req.json();
   let params = {};
-  try {
-    const commit = data.commits[0] as any;
-    const repo_url = data.repository.url as string;
-    const commit_url = commit.url;
-    const raw_message = commit.message as string;
-    const parsed = parseCommitMessge(raw_message)!;
-    const commit_user = commit.committer.username;
+  let results = [];
+  for (let commit of data.commits) {
+    try {
+      const commit = data.commits[0] as any;
+      const repo_url = data.repository.url as string;
+      const commit_url = commit.url;
+      const raw_message = commit.message as string;
+      const parsed = parseCommitMessge(raw_message)!;
+      if (parsed) {
+        const commit_user = commit.committer.username;
 
-    const pair = Secp256k1Keypair.fromSecretKey(process.env.PRIVATE_KEY!);
-    const packageAddress = process.env.NEXT_PUBLIC_PACKAGE_ADDRESS;
-    const url = getRoochNodeUrl(process.env.NEXT_PUBLIC_NETWORK as any);
-    console.log(`rpc url is ${url}`);
+        const pair = Secp256k1Keypair.fromSecretKey(process.env.PRIVATE_KEY!);
+        const packageAddress = process.env.NEXT_PUBLIC_PACKAGE_ADDRESS;
+        const url = getRoochNodeUrl(process.env.NEXT_PUBLIC_NETWORK as any);
+        console.log(`rpc url is ${url}`);
 
-    const client = new RoochClient({
-      url,
-    });
+        const client = new RoochClient({
+          url,
+        });
 
-    params = {
-      target: `${packageAddress}::developer::commit`,
-      args: [
-        parsed.commitAddress,
-        repo_url,
-        commit_url,
-        parsed.messageBody,
-        commit_user,
-        parsed.msg_signate,
-        parsed.msg_hash,
-      ],
-    };
+        params = {
+          target: `${packageAddress}::developer::commit`,
+          args: [
+            parsed.commitAddress,
+            repo_url,
+            commit_url,
+            parsed.messageBody,
+            commit_user,
+            parsed.msg_signate,
+            parsed.msg_hash,
+          ],
+        };
 
-    const tx = new Transaction();
-    tx.callFunction({
-      target: `${packageAddress}::developer::commit`,
-      args: [
-        Args.address(parsed.commitAddress),
-        Args.string(repo_url),
-        Args.string(commit_url),
-        Args.string(parsed.messageBody),
-        Args.string(commit_user),
-        Args.string(parsed.msg_signate),
-        Args.string(parsed.msg_hash),
-      ],
-    });
+        const tx = new Transaction();
+        tx.callFunction({
+          target: `${packageAddress}::developer::commit`,
+          args: [
+            Args.address(parsed.commitAddress),
+            Args.string(repo_url),
+            Args.string(commit_url),
+            Args.string(parsed.messageBody),
+            Args.string(commit_user),
+            Args.string(parsed.msg_signate),
+            Args.string(parsed.msg_hash),
+          ],
+        });
 
-    const result = await client.signAndExecuteTransaction({
-      transaction: tx,
-      signer: pair,
-    });
-    return Response.json({
-      result,
-    });
-  } catch (error) {
-    return Response.json({ error, params });
+        const result = await client.signAndExecuteTransaction({
+          transaction: tx,
+          signer: pair,
+        });
+        results.push(result);
+      }
+    } catch (error) {
+      return Response.json({ error, params });
+    }
   }
+  return Response.json({ results });
 };
